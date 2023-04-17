250cfb59c3bd005adccea9631dd4dd60fef243ff
Aldo Ortega <aortegay@fiu.edu>
2023-04-17 09:25:06 -0400

    Added requests

--- main.py
+++ main.py
@@ -2,8 +2,13 @@
 
 This NApp implements Oplenflow multi tables
 """
+# pylint: disable=unused-argument
+import pathlib
 
-from kytos.core import KytosNApp, log
+from flask import jsonify
+
+from kytos.core import KytosNApp, log, rest
+from kytos.core.helpers import load_spec, validate_openapi
 
 
 class Main(KytosNApp):
@@ -11,6 +16,8 @@ class Main(KytosNApp):
 
     This class is the entry point for this NApp.
     """
+
+    spec = load_spec(pathlib.Path(__file__).parent / "openapi.yml")
 
     def setup(self):
         """Replace the '__init__' method for the KytosNApp subclass.
@@ -23,14 +30,39 @@ class Main(KytosNApp):
         log.info("Setup from of_multi_table")
 
     def execute(self):
-        """Run after the setup method execution.
-
-        You can also use this method in loop mode if you add to the above setup
-        method a line like the following example:
-
-            self.execute_as_loop(30)  # 30-second interval.
-        """
+        """Execute once when the napp is running."""
         log.info("Executing of_multi_table")
+
+    @rest("/v1/pipeline", methods=["POST"])
+    @validate_openapi(spec)
+    def add_pipeline(self, data):
+        """Add pipeline"""
+        return jsonify("Operation successful"), 200
+
+    @rest("/v1/pipeline", methods=["GET"])
+    def list_pipelines(self):
+        """List pipelines"""
+        return jsonify("Operation successful"), 200
+
+    @rest("/v1/pipeline/<pipeline_id>", methods=["GET"])
+    def get_pipeline(self, pipeline_id):
+        """Get pipeline by pipeline_id"""
+        return jsonify("Operation successful"), 200
+
+    @rest("/v1/pipeline/<pipeline_id>", methods=["DELETE"])
+    def delete_pipeline(self, pipeline_id):
+        """Delete pipeline by pipeline_id"""
+        return jsonify("Operation successful"), 200
+
+    @rest("/v1/pipeline/<pipeline_id>/enable", methods=["POST"])
+    def enable_pipeline(self, pipeline_id):
+        """Enable pipeline"""
+        return jsonify("Operation successful"), 201
+
+    @rest("/v1/pipeline/<pipeline_id>/disable", methods=["POST"])
+    def disable_pipeline(self, pipeline_id):
+        """Disable pipeline"""
+        return jsonify("Operation successful"), 201
 
     def shutdown(self):
         """Run when your NApp is unloaded.
--- tests/unit/test_main.py
+++ tests/unit/test_main.py
@@ -1,18 +1,93 @@
 """Test the Main class"""
 from unittest import TestCase
 
-from kytos.lib.helpers import get_controller_mock
+from kytos.lib.helpers import get_controller_mock, get_test_client
+from napps.kytos.of_multi_table.main import Main
 
 
 class TestMain(TestCase):
     """Test the Main class"""
 
+    API_URL = "http://localhost:8181/api/kytos/of_multi_table"
+
     def setUp(self):
         """Execute steps before each test"""
-        from napps.kytos.of_multi_table.main import Main
-        self.napp = Main(get_controller_mock)
+        self.napp = Main(get_controller_mock())
 
-    def test_temporal(self):
-        """Temporal test to be removed later"""
-        name = "Kytos"
-        self.assertEqual("Kytos", name)
+    def test_add_pipeline(self):
+        """Test adding a pipeline"""
+        payload = {
+            "status": "disabled",
+            "multi_table": [{
+                    "description": "Table for testing",
+                    "table_miss_flow": {
+                        "priority": 0,
+                        "match": {},
+                        "instructions": [{
+                            "instruction_type": "goto_table",
+                            "table_id": 1
+                        }]
+                    },
+                    "napps_table_groups": {
+                        "of_lldp": ["base"]
+                    }
+                },
+            ]
+        }
+        api = get_test_client(self.napp.controller, self.napp)
+        url = f"{self.API_URL}/v1/pipeline"
+        response = api.post(url, json=payload)
+        self.assertEqual(response.status_code, 200)
+
+    def test_add_pipeline_error_empty_json(self):
+        """Test adding pipeline with an empty JSON"""
+        api = get_test_client(self.napp.controller, self.napp)
+        url = f"{self.API_URL}/v1/pipeline"
+        response = api.post(url, json={})
+        self.assertEqual(response.status_code, 400)
+
+    def test_add_pipeline_error_empty_content(self):
+        """Test adding pipeline with no content"""
+        api = get_test_client(self.napp.controller, self.napp)
+        url = f"{self.API_URL}/v1/pipeline"
+        response = api.post(url)
+        self.assertEqual(response.status_code, 415)
+
+    def test_list_pipelines(self):
+        """Test list pipelines"""
+        api = get_test_client(self.napp.controller, self.napp)
+        url = f"{self.API_URL}/v1/pipeline"
+        response = api.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_get_pipeline(self):
+        """Test get a pipeline"""
+        api = get_test_client(self.napp.controller, self.napp)
+        pipeline_id = "test_id"
+        url = f"{self.API_URL}/v1/pipeline/{pipeline_id}"
+        response = api.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_delete_pipeline(self):
+        """Test delete a pipeline"""
+        api = get_test_client(self.napp.controller, self.napp)
+        pipeline_id = "test_id"
+        url = f"{self.API_URL}/v1/pipeline/{pipeline_id}"
+        response = api.delete(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_enable_pipeline(self):
+        """Test enable a pipeline"""
+        api = get_test_client(self.napp.controller, self.napp)
+        pipeline_id = "test_id"
+        url = f"{self.API_URL}/v1/pipeline/{pipeline_id}/enable"
+        response = api.post(url)
+        self.assertEqual(response.status_code, 201)
+
+    def test_disable_pipeline(self):
+        """Test disable a pipeline"""
+        api = get_test_client(self.napp.controller, self.napp)
+        pipeline_id = "test_id"
+        url = f"{self.API_URL}/v1/pipeline/{pipeline_id}/disable"
+        response = api.post(url)
+        self.assertEqual(response.status_code, 201)
